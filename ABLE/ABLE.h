2f30a36791c13cc959e6200496caafff8e7b3263
Alessio Orlando <a.orlando@onetech.it>
2017-05-31 14:39:31 +0200

    Initial Commit

---
+++ ABLE/ABLE.h
@@ -0,0 +1,19 @@
+//
+//  ABLE.h
+//  ABLE
+//
+//  Created by Alessio Orlando on 31/05/17.
+//  Copyright © 2017 Alessio Orlando. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ABLE.
+FOUNDATION_EXPORT double ABLEVersionNumber;
+
+//! Project version string for ABLE.
+FOUNDATION_EXPORT const unsigned char ABLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABLE/PublicHeader.h>
+
+
